f2dd593e73a99096960c7f3c8f092466509b76f3
MerlinWe <merlin.s.weiss@gmail.com>
2024-06-28 15:50:41 +0200

    Improvements in random forest workflow, correction in calculations of shapley values

--- analysis/traits/traits_rf_improved.R
+++ analysis/traits/traits_rf_improved.R
@@ -25,145 +25,142 @@
 # 			•	Stratify data by ecoregion and temperature quantiles.
 # 			•	Fit separate models for each stratum using the optimal hyperparameters and compare results.
 
+rm(list = ls()) # clean environment 
+
+# Load necessary libraries
+library(DT)
 library(caret)
 library(ranger)
 library(pdp)
+library(rsample)
 library(fastshap)
 library(doParallel)
 library(ggbeeswarm)
 library(tidyverse)
 
-# Clear the workspace
-rm(list = ls())
+# Read data and make some minor adjustments
+data <- read_csv("/Volumes/ritd-ag-project-rd01pr-dmayn10/merlin/data/fia_traits/traits_rf_clean.csv",
+								 col_types = c("d","d","d","d","d","d","d","d","d","d","f","f","d","d","d","d","d","d","d","d","d","d","d","d")) %>%
+	rename_with(~ gsub("wmean_", "", .), starts_with("wmean_")) %>%
+	rename_with(~ gsub("_015cm", "", .), ends_with("_015cm")) %>%
+	rename(min_temperature = min_temperature_of_coldest_month,
+				 max_temperature = max_temperature_of_warmest_month)
 
-# Load the data
-traits <- read_csv("/Volumes/ritd-ag-project-rd01pr-dmayn10/merlin/data/fia_traits/traits_rf_clean.csv")
+data <- data %>% 
+	filter(standage < 150) %>% # filter standage? 
+	sample_n(1000) # only for code development 
 
 # Split into training and test sets
 set.seed(42)
-data <- traits %>% 
-	filter(standage < 150) %>%
-	sample_n(1000) # only for code development 
+split <- initial_split(data, prop = 0.8)
 
-train_indices <- sample(1:nrow(data), size = 0.8 * nrow(data))
-train_data <- data[train_indices, ]
-test_data <- data[-train_indices, ]
+# Extract training and testing datasets
+train_data <- training(split)
+test_data  <- testing(split)
 
 # Define the traits and covariates
-traits <- c("wmean_wood_density", "wmean_bark_thickness", "wmean_conduit_diam", "wmean_leaf_n", 
-						"wmean_specific_leaf_area", "wmean_seed_dry_mass", "wmean_shade_tolerance", "wmean_height")
+traits <- c("wood_density", "bark_thickness", "conduit_diam", "leaf_n", 
+						"specific_leaf_area", "seed_dry_mass", "shade_tolerance", "height")
 
+# Define numeric covariates and all covariates
 covariates <- c("standage", "annual_mean_temperature", "annual_precipitation", 
-								"temperature_seasonality", "mean_diurnal_range", 
-								"min_temperature_of_coldest_month", "max_temperature_of_warmest_month", 
-								"elevation", "pop_density", "sand_content_015cm", 
-								"soil_ph_015cm", "water_capacity_015cm")
+								"temperature_seasonality", "mean_diurnal_range", "min_temperature", 
+								"max_temperature", "elevation", "pop_density", 
+								"sand_content", "soil_ph", "water_capacity")
 
-# Define a grid of hyperparameters including mtry, min.niode.size, and split rule
+# Define a grid of hyperparameters including mtry and min.node.size
 hyper_grid <- expand.grid(
 	mtry = seq(2, length(covariates), by = 2),
-	splitrule = "variance",
-	min.node.size = c(1, 5, 10)
+	min.node.size = c(1, 5, 10),
+	splitrule = "variance"
 )
 
-# Function to perform cross-validation, find the best hyperparameters, and capture performance metrics
+# Perform cross-validation, find the best hyperparameters, capture performance metrics
 tune_rf_model <- function(trait, data, covariates, hyper_grid) {
+	
+	# Define model formula
 	formula <- as.formula(paste(trait, "~", paste(covariates, collapse = " + ")))
 	
-	train_control <- trainControl(method = "cv", number = 5, savePredictions = "final", summaryFunction = defaultSummary)
+	# Set training framework
+	train_control <- trainControl(
+		method = "cv", 
+		number = 5, 
+		savePredictions = "final", 
+		summaryFunction = defaultSummary
+	)
 	
+	# Train models
 	model <- train(
-		formula, data = data,
+		formula, 
+		data = data,
 		method = "ranger",
 		trControl = train_control,
 		tuneGrid = hyper_grid,
 		importance = "permutation"
 	)
 	
-	# Extract performance metrics
+	# Get performance metrics
 	results <- model$results
 	best_tune <- model$bestTune
 	results <- results %>%
-		mutate(trait = trait, 
-					 best = rowSums(sapply(names(best_tune), function(param) results[[param]] == best_tune[[param]])) == length(best_tune))
+		mutate(
+			trait = trait, 
+			best = rowSums(sapply(names(best_tune), 
+														function(param) results[[param]] == best_tune[[param]])) == length(best_tune)
+		)
 	
 	return(list(model = model, results = results))
 }
 
-# Perform hyperparameter tuning for each trait and capture performance metrics
-tuned_models <- lapply(traits, function(trait) tune_rf_model(trait, train_data, covariates, hyper_grid))
+# Register parallel cores before tuning and calculating Shapley values
+num_cores <- detectCores() - 2
+registerDoParallel(num_cores)
 
-# Extract the best models and performance metrics
-best_models <- lapply(tuned_models, function(x) x$model$finalModel)
-performance_metrics <- do.call(rbind, lapply(tuned_models, function(x) x$results))
+# Perform hyperparameter tuning for each trait and get performance metrics
+tuned_models <- traits %>%
+	map(~ tune_rf_model(.x, train_data, covariates, hyper_grid))
 
-# •	RMSE: Root Mean Squared Error. This is a measure of the average deviation of the predicted values from the actual values. It is calculated as the square root of the average of squared differences between predicted and actual values. Lower values indicate better model performance.
-# •	Rsquared: R-squared. This is a measure of how well the model’s predictions approximate the actual data. It represents the proportion of variance in the dependent variable that is predictable from the independent variables. Values range from 0 to 1, with higher values indicating better model performance.
-# •	MAE: Mean Absolute Error. This is the average of the absolute differences between predicted and actual values. It provides a measure of how far the predictions are from the actual values on average. Lower values indicate better model performance.
-# •	RMSESD: Standard Deviation of Root Mean Squared Error. This measures the variability of the RMSE across different cross-validation folds. Lower values indicate more consistent model performance.
-# •	RsquaredSD: Standard Deviation of R-squared. This measures the variability of the R-squared values across different cross-validation folds. Lower values indicate more consistent model performance.
-# •	MAESD: Standard Deviation of Mean Absolute Error. This measures the variability of the MAE across different cross-validation folds. Lower values indicate more consistent model performance.
+# Extract the best model for every trait
+best_models <- tuned_models %>%
+	map(~ .x$model$finalModel)
 
-# Define prediction function for fastshap
+# Extract performance metrics
+performance_metrics <- tuned_models %>%
+	map_df(~ .x$results) %>%
+	mutate(across(where(is.numeric), ~ round(.x, 2))) %>%
+	arrange(trait, best)
+
+# Prediction function for fastshap
 predict_fn <- function(object, newdata) {
 	predict(object, data = newdata)$predictions
 }
 
-# Calculate Shapley values
-registerDoParallel(detectCores() - 1)
-shap_values <- lapply(best_models, function(model) {
-	fastshap::explain(model, X = test_data[, covariates, drop = FALSE], pred_wrapper = predict_fn, nsim = 100, parallel = TRUE)
-})
-
-# Define a function to plot Shapley values
-plot_shap_values <- function(shap_values, covariates, trait) {
-	# Reshape Shapley values to a long format
-	shap_df <- as.data.frame(shap_values)
-	shap_df$id <- 1:nrow(shap_df)
-	shap_long <- tidyr::gather(shap_df, key = "feature", value = "shap", -id)
-	
-	# Ensure the features match the covariates
-	shap_long$feature <- rep(covariates, each = nrow(shap_df))
-	
-	# Plotting
-	ggplot(shap_long, aes(x = feature, y = shap, color = shap)) +
-		geom_quasirandom(alpha = 0.5) +
-		scale_color_viridis_c(option = "viridis") +
-		coord_flip() +
-		labs(title = paste(trait), x = "Feature", y = "Shapley Value") +
-		theme_bw() +
-		theme(text = element_text(family = "Palatino"),
-					legend.position = "none")
+# Calculate Shapley values for each trait model using parallel processing
+shap_values <- foreach(i = seq_along(best_models), .combine = 'rbind', .packages = c('fastshap', 'dplyr')) %dopar% {
+	model <- best_models[[i]]
+	trait <- traits[i]
+	shap_values <- fastshap::explain(model, X = test_data %>% select(all_of(covariates)) %>% as.matrix(),
+																	 pred_wrapper = predict_fn, nsim = 100, parallel = TRUE)
+	shap_values <- as.data.frame(shap_values)
+	shap_values$trait <- trait
+	shap_values
 }
 
-plots <- lapply(seq_along(traits), function(i) plot_shap_values(shap_values[[i]], covariates, traits[i]))
-combined_plot <- cowplot::plot_grid(plotlist = plots, ncol = 4, nrow = 2)
-print(combined_plot)
+# Stop the parallel backend
+stopImplicitCluster()
 
+# Reshape the combined data frame to long format
+shap_values_long <- shap_values %>%
+	pivot_longer(cols = -c(trait), names_to = "feature", values_to = "shap") %>%
+	as_tibble()
 
-## Plot predictions using partial residual curves
-plot_partial_dependence <- function(model, data, trait, covariate) {
-	
-	# Generate partial dependence data
-	pd_data <- partial(model, pred.var = covariate, train = data, grid.resolution = 100)
-	
-	# Convert to data frame for ggplot
-	pd_df <- as.data.frame(pd_data)
-	
-	# Plot partial dependence data
-	ggplot(pd_df, aes(x = !!sym(covariate), y = yhat)) +
-		geom_line(color = "blue") +
-		labs(title = paste("Partial Dependence of", trait, "on", covariate),
-				 x = covariate, y = "Predicted Value") +
-		theme_bw() +
-		theme(text = element_text(family = "Palatino"))
-}
-
-# Generate and plot partial dependence for each trait
-partial_plots <- lapply(seq_along(traits), function(i) {
-	plot_partial_dependence(best_models[[i]], test_data, traits[i], "standage")
-})
-
-# Combine all plots into a single plot
-combined_partial_plot <- cowplot::plot_grid(plotlist = partial_plots, ncol = 4, nrow = 2)
-print(combined_partial_plot)
+shap_values_long %>%
+	ggplot( aes(x = feature, y = shap, color = shap)) +
+	geom_quasirandom(alpha = 0.5) +
+	facet_wrap(~trait, ncol = 4, nrow = 2) +
+	scale_color_viridis_c(option = "viridis") +
+	coord_flip() +
+	labs(x = "Feature", y = "Shapley Value") +
+	theme_bw() +
+	theme(text = element_text(family = "Palatino"),
+				legend.position = "none")
