6eb3a54bc4c1af59d5cf6985820c2a752e68db3e
MerlinWe <merlin.s.weiss@gmail.com>
2024-07-25 21:53:43 +0200

    Hypothesis plot

--- analysis/hypothesis.R
+++ analysis/hypothesis.R
@@ -3,21 +3,21 @@ library(ggplot2)
 quadrants <- data.frame(
 	x = c(1, 1, 3, 3),
 	y = c(1, 3, 1, 3),
-	label = c("Weakly determined by\nenvironmental selection", 
-						"Strongly determined by\nenvironmental selection", 
-						"Weakly determined\nby biotic drivers", 
-						"Strongly determined\nby biotic drivers"))
+	label = c("Strong environmental filtering,\nhigh stochasitcity", 
+						"Strong environmental filtering,\nhigh determinism", 
+						"No environmental filtering\n strongly affected by biotic drivers",
+						"Partial environmental filtering,\nstrong biotic residual effect"))
 
 hypo <- ggplot() +
 	geom_rect(data = quadrants, aes(xmin = ifelse(x == 1, -1.95, 0.05), xmax = ifelse(x == 3, 1.95, -0.05), 
 																	ymin = ifelse(y == 1, -1.95, 0.05), ymax = ifelse(y == 3, 1.95, -0.05), 
-																	fill = label), alpha = 0.2, colour = "black", linewidth = .3) +
+																	fill = label), alpha = 0.3, colour = "black", linewidth = .3) +
 	geom_text(data = quadrants, aes(x = ifelse(x == 1, -1, 1), y = ifelse(y == 1, -1, 1), 
-																	label = label), hjust = "center", vjust = "center", size = 3.5, family = "sans") +
-	scale_fill_manual(values = c("darkgreen","midnightblue", "palegreen", "lightskyblue")) +
+																	label = label), hjust = "center", vjust = "center", size = 2.5, family = "sans") +
+	scale_fill_manual(values = c("palegreen","darkgreen", "midnightblue", "lightskyblue")) +
 	xlim(-2, 2) +
 	ylim(-2, 2) +
-	xlab("Trait change over succession") +
+	xlab("Signal across successional time") +
 	ylab("Determinism") +
 	theme_classic() +
 	theme(legend.position = "none",
@@ -49,3 +49,4 @@ ggsave(filename = "/Users/serpent/Documents/MSc/Thesis/Code/analysis/plots/hypot
 			 height = 80, 
 			 units = "mm", 
 			 dpi = 600)
+
